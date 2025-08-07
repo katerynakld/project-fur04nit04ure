38324a3db7c8a105120a0bd90a9fe237a0a32305
Kateryna Koliada <6parkseolhwa6@gmail.com>
2025-08-07 21:08:05 +0200

    Fix errors

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -4,8 +4,4 @@ import 'accordion-js/dist/accordion.min.css';
 new Accordion('.accordion-container', {
   duration: 250,
   showMultiple: false,
-
-  onOpen: function (currentElement) {
-    console.log(currentElement);
-  },
 });
