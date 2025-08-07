8cad8f5d4e592156a7a6caaa73bcb4cf65ef8448
Kateryna Koliada <6parkseolhwa6@gmail.com>
2025-08-07 15:43:02 +0300

    Finish styling

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -2,8 +2,9 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 new Accordion('.accordion-container', {
-  duration: 400,
-  showMultiple: true,
+  duration: 250,
+  showMultiple: false,
+
   onOpen: function (currentElement) {
     console.log(currentElement);
   },
